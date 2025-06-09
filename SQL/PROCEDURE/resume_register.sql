239cdf06477b69e64f5bbd45a2b7f5c7734ad185
chaserChoi <chlwogur2010@gmail.com>
2025-06-09 10:14:29 +0900

    choi - 이력서 등록 프로시저 수정(제목 중복 기능 추가)

--- SQL/PROCEDURE/resume_register.sql
+++ SQL/PROCEDURE/resume_register.sql
@@ -8,10 +8,16 @@ create procedure RESUME_001(
 )
 begin
     if exists(select 1 from member where id = memberIdInput and member_type = 'user' and state = 'online') then
-        insert into resume (title, contents, member_id)
-        values (titleInput, contentsInput, memberIdInput);
+        if (select 1 from resume where member_id = memberIdInput and title = titleInput) then
+            -- 이미 등록된 이력서 제목이 존재하는 경우 오류 발생
+            signal sqlstate '45000' set message_text = '이미 등록된 이력서 제목이 존재합니다.';
+        else
+            insert into resume (title, contents, member_id)
+                values (titleInput, contentsInput, memberIdInput);
 
-        select LAST_INSERT_ID() as resume_id;
+            -- 이력서 등록 성공 메세지
+            select '이력서 등록 완료!' as message;
+        end if;
     else
         -- 개인 계정이 아닌 경우 오류 발생
         signal sqlstate '45000' set message_text = '개인 계정만 등록 가능!';
